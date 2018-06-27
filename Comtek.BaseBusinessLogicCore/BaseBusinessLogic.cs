02811b58202d36ecce91a24ee646ed81b41db186
Craig Verburgh <cverburgh@comtekadvanced.com>
2018-06-27 16:05:58 -0400

    Init

---
+++ Comtek.BaseBusinessLogicCore/BaseBusinessLogic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using AES.HelpersCore;
+using Comtek.DataOperationResultCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Comtek.BaseBusinessLogicCore
+{
+    public abstract class BaseBusinessLogic<T> where T : DbContext
+    {
+        private readonly string successMessage;
+        private readonly string failMessage;
+        public T Context { get; }
+
+        protected BaseBusinessLogic(T context, string appName)
+        {
+            Context = context;
+            successMessage = $"Changes saved to {appName} database";
+            failMessage = $"There was an error saving the changes to the {appName} database.";
+        }
+
+        public DataOperationResult Save()
+        {
+            var result = new DataOperationResult();
+            try
+            {
+                Context.SaveChanges();
+                result.Success = true;
+                result.Messages.Add(successMessage);
+            }
+            catch (Exception ex)
+            {
+                result.Success = false;
+                result.Messages.Add(failMessage);
+                result.Messages.Add(ErrorHelper.GetExceptionMessage(ex));
+            }
+            return result;
+        }
+
+        public DataOperationResult SaveAsync()
+        {
+            var result = new DataOperationResult();
+            try
+            {
+                Context.SaveChangesAsync();
+                result.Success = true;
+                result.Messages.Add(successMessage);
+            }
+            catch (Exception ex)
+            {
+                result.Success = false;
+                result.Messages.Add(failMessage);
+                result.Messages.Add(ErrorHelper.GetExceptionMessage(ex));
+            }
+            return result;
+        }
+    }
+}
--- Comtek.BaseBusinessLogicCore/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Comtek.BaseBusinessLogicCore
-{
-    public class Class1
-    {
-    }
-}
